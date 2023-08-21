464467a32b41ca64659253f7e0b84f3554a97d72
hkup859 <20144454+hkup859@users.noreply.github.com>
2023-08-21 20:48:19 +0000

    Add support for other AES-CBC encryption

--- index.ts
+++ index.ts
@@ -1,27 +1,32 @@
 import crypto from 'crypto'
 
-const SUPPORTED_ALGORITHMS = {
-  AES_256_CBC: 'aes-256-cbc'
+export const SUPPORTED_ALGORITHMS = {
+  AES_CBC_256: { name: 'aes-256-cbc', ivLength: 16, keyLength: 32 },
+  AES_CBC_192: { name: 'aes-192-cbc', ivLength: 16, keyLength: 24 },
+  AES_CBC_128: { name: 'aes-128-cbc', ivLength: 16, keyLength: 16 }
+  // AES_256_GCM: 'aes-256-gcm'
 } as const
 
-type ObjectValues<T> = T[keyof T]
-export type Algorithm = ObjectValues<typeof SUPPORTED_ALGORITHMS>
+// export type Algorithm = (typeof)[keyof typeof SUPPORTED_ALGORITHMS]
+export type Algorithm = keyof typeof SUPPORTED_ALGORITHMS
 
 // AES-256-CBC Encryption
 export const encrypt = (buffer: Buffer, password: string, algorithm: Algorithm) => {
-  const initVector = crypto.randomBytes(16)
-  const generatedKey = crypto.scryptSync(password, initVector, 32)
-  const cipher = crypto.createCipheriv(algorithm, generatedKey, initVector);
+  const algorithmDetails = SUPPORTED_ALGORITHMS[algorithm]
+  const initVector = crypto.randomBytes(algorithmDetails.ivLength)
+  const generatedKey = crypto.scryptSync(password, initVector, algorithmDetails.keyLength)
+  const cipher = crypto.createCipheriv(algorithmDetails.name, generatedKey, initVector);
   const encryptedData = Buffer.concat([initVector, cipher.update(buffer), cipher.final()]);
   return encryptedData
 }
 
 // AES-256-CBC Decryption
 export const decrypt = (encryptedData: Buffer, password: string, algorithm: Algorithm) => {
-  const initVector: Buffer = encryptedData.slice(0, 16)
-  const encryptedBuffer: Buffer = encryptedData.slice(16)
-  const generatedKey = crypto.scryptSync(password, initVector, 32)
-  const decipher = crypto.createDecipheriv(algorithm, generatedKey, initVector);
+  const algorithmDetails = SUPPORTED_ALGORITHMS[algorithm]
+  const initVector: Buffer = encryptedData.slice(0, algorithmDetails.ivLength)
+  const encryptedBuffer: Buffer = encryptedData.slice(algorithmDetails.ivLength)
+  const generatedKey = crypto.scryptSync(password, initVector, algorithmDetails.keyLength)
+  const decipher = crypto.createDecipheriv(algorithmDetails.name, generatedKey, initVector);
   const decryptedData = Buffer.concat([decipher.update(encryptedBuffer), decipher.final()])
   return decryptedData
 }
--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,8 +2,8 @@ import { encrypt, decrypt } from '../index'
 describe('Initial Test Suite', () => {
   it('Encrypt String', () => {
     const secretMessage = 'This is TOP secret'
-    const encryptedMessage = encrypt(Buffer.from(secretMessage), 'testpassword', 'aes-256-cbc')
-    const decryptedMessage = decrypt(encryptedMessage, 'testpassword', 'aes-256-cbc').toString('utf-8')
+    const encryptedMessage = encrypt(Buffer.from(secretMessage), 'testpassword', 'AES_CBC_256')
+    const decryptedMessage = decrypt(encryptedMessage, 'testpassword', 'AES_CBC_256').toString('utf-8')
     expect(secretMessage).toEqual(decryptedMessage)
   })
 })
